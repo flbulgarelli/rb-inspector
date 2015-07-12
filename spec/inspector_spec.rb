bda4ed39a63e927a106c66d44bc7316ffae57e21
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-12 19:46:53 -0300

    Adding test for multiple declarations

--- spec/inspector_spec.rb
+++ spec/inspector_spec.rb
@@ -22,4 +22,13 @@ describe Inspector do
     it { expect(code.has_binding? 'x').to be false }
   end
 
+  context 'when multiple declarations' do
+    let(:code) { Inspector::Code.parse 'def bar; 4; end; x = 2; z = 7' }
+
+    it { expect(code.has_binding? 'bar').to be true }
+    it { expect(code.has_binding? 'x').to be true }
+    it { expect(code.has_binding? 'z').to be true }
+    it { expect(code.has_binding? 'y').to be false }
+  end
+
 end
