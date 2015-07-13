e85a2aa6cbc82b1df54fdbfa0b5a09e1936d83c7
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-12 23:20:34 -0300

    Adding more (failing) tests

--- spec/inspector_spec.rb
+++ spec/inspector_spec.rb
@@ -29,70 +29,85 @@ describe Inspector do
 
   context 'when no binding' do
     let(:code) { Inspector::Code.parse 'x + 1' }
-    it { expect(code.has_binding? 'foo').to be false }
-    it { expect(code.has_binding? 'x').to be false }
+    it { expect(code.has_binding? :foo).to be false }
+    it { expect(code.has_binding? :x).to be false }
   end
 
   context 'when variable declaration' do
     let(:code) { Inspector::Code.parse 'x = 2' }
 
-    it { expect(code.has_binding? 'foo').to be false }
-    it { expect(code.has_binding? 'x').to be true }
+    it { expect(code.has_binding? :foo).to be false }
+    it { expect(code.has_binding? :x).to be true }
   end
 
   context 'when method declaration' do
     let(:code) { Inspector::Code.parse 'def foo; x = 4; x ; end' }
 
-    it { expect(code.has_binding? 'foo').to be true }
-    it { expect(code.has_binding? 'x').to be false }
+    it { expect(code.has_binding? :foo).to be true }
+    it { expect(code.has_binding? :x).to be false }
   end
 
   context 'when class declaration' do
     let(:code) { Inspector::Code.parse 'class Foo; def bar; end; end' }
 
-    it { expect(code.has_binding? 'Foo').to be true }
-    it { expect(code.has_binding? 'bar').to be false }
+    it { expect(code.has_binding? :Foo).to be true }
+    it { expect(code.has_binding? :bar).to be false }
   end
 
   context 'when scoped class declaration' do
     let(:code) { Inspector::Code.parse 'class X::Foo; def bar; end; end' }
 
-    it { expect(code.has_binding? 'Foo').to be false }
+    it { expect(code.has_binding? :Foo).to be false }
   end
 
   context 'when top level class declaration' do
     let(:code) { Inspector::Code.parse 'class ::Foo; def bar; end; end' }
 
-    it { expect(code.has_binding? 'Foo').to be true }
+    it { expect(code.has_binding? :Foo).to be true }
   end
 
   context 'when module declaration' do
     let(:code) { Inspector::Code.parse 'module Foo; def bar; end; end' }
 
-    it { expect(code.has_binding? 'Foo').to be true }
-    it { expect(code.has_binding? 'bar').to be false }
+    it { expect(code.has_binding? :Foo).to be true }
+    it { expect(code.has_binding? :bar).to be false }
+  end
+
+  context 'when method declaration in class' do
+    let(:code) { Inspector::Code.parse 'module Foo; def bar; end; end' }
+
+    it { pending; expect(code.has_binding? :'Foo.bar').to be true }
+    it { expect(code.has_binding? :'Foo.baz').to be false }
+    it { expect(code.has_binding? :'Baz.bar').to be false }
+  end
+
+  context 'when method declaration in object' do
+    let(:code) { Inspector::Code.parse 'foo = Foo.new; def foo.bar; end' }
+
+    it { pending; expect(code.has_binding? :'foo.bar').to be true }
+    it { expect(code.has_binding? :'foo.baz').to be false }
+    it { expect(code.has_binding? :'baz.bar').to be false }
   end
 
   context 'when scoped module declaration' do
     let(:code) { Inspector::Code.parse 'module X::Foo; def bar; end; end' }
 
-    it { expect(code.has_binding? 'Foo').to be false }
+    it { expect(code.has_binding? :Foo).to be false }
   end
 
   context 'when top level module declaration' do
     let(:code) { Inspector::Code.parse 'module ::Foo; def bar; end; end' }
 
-    it { expect(code.has_binding? 'Foo').to be true }
+    it { expect(code.has_binding? :Foo).to be true }
   end
-
 
   context 'when multiple declarations' do
     let(:code) { Inspector::Code.parse 'def bar; 4; end; x = 2; z = 7' }
 
-    it { expect(code.has_binding? 'bar').to be true }
-    it { expect(code.has_binding? 'x').to be true }
-    it { expect(code.has_binding? 'z').to be true }
-    it { expect(code.has_binding? 'y').to be false }
+    it { expect(code.has_binding? :bar).to be true }
+    it { expect(code.has_binding? :x).to be true }
+    it { expect(code.has_binding? :z).to be true }
+    it { expect(code.has_binding? :y).to be false }
   end
 
 end
